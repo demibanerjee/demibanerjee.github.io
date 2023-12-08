448f199728b23759f38e127382aa8ceab6d5e3b3
demibanerjee <45940510+demibanerjee@users.noreply.github.com>
2023-12-07 23:12:26 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+// JavaScript for Carousel
+function initializeCarousel() {
+    // Code to initialize a carousel for projects
+}
+
+// JavaScript for Modal Pop-ups
+function initializeModals() {
+    // Code to initialize modals for detailed views
+}
+
+// JavaScript for Hover Effects
+function addHoverEffects() {
+    // Code to add hover effects to certain elements
+}
+
+// Initialize functions on window load
+window.onload = function() {
+    initializeCarousel();
+    initializeModals();
+    addHoverEffects();
+};
